c229421a8704976fc7e43fd4f0b3fffab092aa90
Iara Moreiras Franco <121463061+iaragab@users.noreply.github.com>
2025-07-04 18:42:57 +0200

    archivos del proyecto

---
+++ DKproyectDK.js
@@ -0,0 +1,8 @@
+
+  const menuBtn = document.getElementById('menuExpenBTN');
+  const navMenu = document.getElementById('navMenu');
+
+  menuBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+  });
+
